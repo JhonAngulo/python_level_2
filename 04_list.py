aee2b948fa72a9b20eb2890c8f04907780496c07
JhonAngulo <jhonmanuelm89@gmail.com>
2024-05-14 21:10:03 -0500

    update 14/05/24

---
+++ 04_list.py
@@ -0,0 +1,29 @@
+# numbers = []
+
+# for element in range(1, 11):
+#     numbers.append(element)
+
+# print(numbers)
+
+# numbers_v2 = [element for element in range(1, 11)]
+# print(numbers_v2)
+
+# numbers = []
+
+# for element in range(1, 11):
+#     numbers.append(element * 2)
+
+# print(numbers)
+
+# numbers_v2 = [element * 2 for element in range(1, 11)]
+# print(numbers_v2)
+
+numbers = []
+
+for element in range(1, 11):
+    numbers.append(element * 2)
+
+print(numbers)
+
+numbers_v2 = [element * 2 for element in range(1, 11)]
+print(numbers_v2)
---
+++ 07_func.py
@@ -0,0 +1,20 @@
+print('Hola')
+
+
+def my_print(text):
+    # print('This is my print')
+    print(text * 2)
+
+my_print('Este es mi texto')
+my_print('Hola')
+
+a = 10
+b = 90
+
+c = a + b
+
+def suma(a, b):
+    print(a + b)
+
+suma(100, 100)
+suma(10, 5)
